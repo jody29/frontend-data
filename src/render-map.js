4d913192a972405a31893de7d045ce803753af0b
Jody Lorist <jodylorist@mbp-van-jody.home>
2021-11-16 01:11:44 +0100

    map of netherlands renders

--- src/render-map.js
+++ src/render-map.js
@@ -1,14 +1,19 @@
+
 import removeArrow from "./modules/filters/remove-arrow.js"
 
-const gemeenten = 'https://cartomap.github.io/nl/rd/gemeente_2021.topojson'
+const gemeenten = 'https://cartomap.github.io/nl/rd/gemeente_2021.geojson'
 const townShips = 'https://www.webuildinternet.com/articles/2015-07-19-geojson-data-of-the-netherlands/townships.geojson'
+const provinces = 'https://www.webuildinternet.com/articles/2015-07-19-geojson-data-of-the-netherlands/provinces.geojson'
 const vaccinatie = 'https://data.rivm.nl/covid-19/COVID-19_vaccinatiegraad_per_gemeente_per_week_leeftijd.json'
+const gemeentes = 'https://raw.githubusercontent.com/deldersveld/topojson/master/countries/netherlands/nl-gemeentegrenzen-2016.json'
 
 let gemeentenData
 let vaccinData = []
 
-let width = 1000,
-height = 900
+
+
+let width = 800,
+height = '100vh'
 
 let canvas = d3.select('#canvas')
 .attr('width', width)
@@ -17,29 +22,32 @@ let canvas = d3.select('#canvas')
 let drawMap = () => {
 
     let center = d3.geoCentroid(gemeentenData)
-    let scale = 150
-    let offset = [width/2, height/2]
-    let projection = d3.geoMercator().scale(10000)
+    let scale = 8500
+    let offset = [scale/10, scale+710]
+    let projection = d3.geoMercator().scale(scale)
 
     let path = d3.geoPath().projection(projection)
 
     canvas.selectAll('path')
     .data(gemeentenData)
     .enter().append('path')
-    .attr('transform', 'translate(-900, 10910)')
+    .attr('transform', `translate(-${scale/10}, ${scale+710})`)
     .attr('d', path)
     .attr('class', 'gemeente')
 
     console.log(gemeentenData)
 }
 
-d3.json(townShips).then(
+d3.json(gemeentes).then(
     (data, error) => {
         if (error) {
             console.log(error) 
         } else {
-            // gemeentenData = topojson.feature(data, data.objects.gemeente_2021).features
-            gemeentenData = data.features
+
+            // gemeentenData = data.features
+            gemeentenData = topojson.feature(data, data.objects.Gemeentegrenzen).features
+            
+            // console.log(topojson.feature(data, data.objects))
             console.log(gemeentenData)
             drawMap()
 
