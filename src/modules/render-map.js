1d0d05cdc6e240863f3035d6b8999290e36404fb
Jody Lorist <jodylorist@mbp-van-jody.home>
2021-11-17 17:24:40 +0100

    REFACTOR: added comments

--- src/modules/render-map.js
+++ src/modules/render-map.js
@@ -1,47 +1,48 @@
-let width = 600,
+let width = 600, // define width and height
 height = '89vh'
 
-let canvas = d3.select('#canvas')
-.attr('width', width)
-.attr('height', height)
+let canvas = d3.select('#canvas') // select the canvas where the map should be rendered
+.attr('width', width) // give the canvas a width
+.attr('height', height) // give the canvas a height
 
-let svg = d3.select('#percentage')
-.append('svg')
-.attr('width', 300)
-.attr('height', 100)
+let svg = d3.select('#percentage') // select div with id #percentage
+.append('svg') // append a svg to it
+.attr('width', 300) // give it a width of 300
+.attr('height', 100) // give it a height of 100
 
-let bgRect = svg.append('rect')
-.attr('width', 300)
-.attr('height', 50)
-.style('opacity', 0)
-.attr('fill', 'dimgray')
+let bgRect = svg.append('rect') // append rectangle to svg
+.attr('width', 300) // give it a default width
+.attr('height', 50) // give it a default height
+.style('opacity', 0) // opacity of the rect to 0
+.attr('fill', 'dimgray') // fill of th rect to dimgray
 
-let fillRect = svg.append('rect')
+let fillRect = svg.append('rect') // this is the progress rectangle
 
-function drawMap(gemeentenData, vaccinData, colors) {
-    let scale = 7500
-    let projection = d3.geoMercator().scale(scale)
+function drawMap(gemeentenData, vaccinData, colors) { // define function with gemeentenData, vaccinData & colors as parameters
+    let scale = 7500 // define scale
+    let projection = d3.geoMercator().scale(scale) // define the map projection with the scale
 
-    let path = d3.geoPath().projection(projection)
+    let path = d3.geoPath().projection(projection) // define the path with geoMercator projection
 
-    canvas.selectAll('path')
-    .data(gemeentenData)
-    .join('path')
-    .attr('transform', `translate(-${scale/8.3}, ${scale+585})`)
-    .attr('d', path)
-    .attr('class', 'gemeente')
+    canvas.selectAll('path') // select all paths (municipalities of Holland)
+    .data(gemeentenData) // geojson file of all municipalities as data
+    .join('path') // create paths for every object from the data and delete if there are too many
+    .attr('transform', `translate(-${scale/8.3}, ${scale+585})`) // due to the fact that te map was going up because of the scale, i had to transform the paths so it would fit inside the canvas
+    .attr('d', path) // d element of path should contain the path coordinates
+    .attr('class', 'gemeente') // assign class gemeente to the path element
     .attr('fill', (d) => {
-        let id = d.properties.gemeentena
+        let id = d.properties.gemeentena // id is the name of the municipality
         let gemeente = vaccinData.find((item) => {
-            return item['region'] === id
+            return item['region'] === id // return all objects from vaccinData that contain the same region name as the id variabel
         })
-        let percentage = gemeente.vaccination
-        d.properties.percentage = percentage
+        let percentage = gemeente.vaccination // assign the vaccination percentage of the municipality to percentage
+        d.properties.percentage = percentage // create object property percentage in the geojson file with the vaccination percentage of the municipality as value
         
-        if (percentage <= 50) {
+        // I could also use a switch statement for this. For this time i decided to use a if/else statement
+        if (percentage <= 50) { // check the percentage and assign the right color to it
             return colors[0].color
         } else if (percentage <= 60) {
-            return colors[1].color
+            return colors[1].color // colors are from the color parameter. These colors are assigned in fetch-map.js
         } else if (percentage <= 70) {
             return colors[2].color
         } else if (percentage <= 80) {
@@ -52,31 +53,31 @@ function drawMap(gemeentenData, vaccinData, colors) {
             return colors[5].color
         }
     })
-    .attr('data-gemeente', (d) => d.properties.gemeentena)
-    .attr('data-value', (d) => d.properties.percentage)
-    .on('click', (e) => {
+    .attr('data-gemeente', (d) => d.properties.gemeentena) // custom attribute with the name of the municipality as value
+    .attr('data-value', (d) => d.properties.percentage) // custom attribute with the vaccination percentage of the municipality as value
+    .on('click', (e) => { // launch event when clicking on a path element
 
-        let value = e.target.getAttribute('data-value')
-        let gemeente = e.target.getAttribute('data-gemeente')
+        let value = e.target.getAttribute('data-value') // get the vaccination percentage of the path the user clicked on
+        let gemeente = e.target.getAttribute('data-gemeente') // get the municipality of the path the user clicked on
 
-        console.log(value)
+        d3.select('#percentage') // select the div with id percentage
+        .style('padding', '2em') // give this div a padding of 2em
+        .select('h2') // select the h2 in the div
+        .text(gemeente) // give this h2 the municipality name as text
 
-        console.log((value * 300) / 100)
+        d3.select('#percentage') // select div with id percentage
+        .select('p') // select p element in the div
+        .text(value + '%') // assign vaccination percentage as text to this p element
 
-        d3.select('#percentage')
-        .style('padding', '2em')
-        .select('h2')
-        .text(gemeente)
+        bgRect.transition() // add a transition to the bgRect
+        .style('opacity', 1) // opacity will go from 0 to 1 now
 
-        bgRect.transition()
-        .style('opacity', 1)
-
-        fillRect.attr('height', 50)
-        .transition()
-        .duration(1000)
-        .attr('width', (value * 300) / 100)
+        fillRect.attr('height', 50) // give the progress rectangle a height of 50
+        .transition() // add a transition
+        .duration(1000) // transition should take a second
+        .attr('width', (value * 300) / 100) // width should go from 0 to the right percentage
         .attr('fill', () => {
-            if (value <= 50) {
+            if (value <= 50) { // allready explained this on row 42
                 return colors[0].color
             } else if (value <= 60) {
                 return colors[1].color
@@ -89,16 +90,11 @@ function drawMap(gemeentenData, vaccinData, colors) {
             } else if (value <= 100) {
                 return colors[5].color
             }
-        })
-
-        d3.select('#percentage')
-        .select('p')
-        .text(value + '%')
-        
+        })  
     })
 }
 
 
 
-export default drawMap
+export default drawMap // export the drawmap function
 
